bfbfddb7b9cf28e9c0800d7f701a07736025cc7b
Maestro28 <bohdan.rybak28@gmail.com>
2022-05-12 08:33:32 +0300

    db settings and book admin py

--- library/settings.py
+++ library/settings.py
@@ -85,6 +85,8 @@ DATABASES = {
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
