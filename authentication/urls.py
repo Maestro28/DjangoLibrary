4602edcc0898adfa617212b21db5b82c3276ea15
Maestro28 <bohdan.rybak28@gmail.com>
2022-05-30 20:22:13 +0300

    passworld change

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from django.urls import reverse_lazy
 
 from . import views
+from django.contrib.auth.views import PasswordChangeDoneView, PasswordChangeView
 
 app_name = 'authentication'
 urlpatterns = [
@@ -8,4 +10,10 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("profile/", views.profile, name="profile"),
     path("accounts/", include("django.contrib.auth.urls"), name='accounts'),
+
+    path('password-change/', PasswordChangeView.as_view(template_name='registration/password_change_form.html',
+                                                        success_url=reverse_lazy('authentication:password_change_done')), name='password_change'),
+    path('password_change_done/', PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), {}, name='password_change_done'),
+
+
 ]
